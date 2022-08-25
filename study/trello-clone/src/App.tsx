af06ea035a6aad5a79ecabb89e3976c64e7071d8
pencilCool <yhtangcoder@gmail.com>
2022-08-25 11:03:13 +0800

    update

--- study/trello-clone/src/App.tsx
+++ study/trello-clone/src/App.tsx
@@ -3,19 +3,15 @@ import { Column } from "./Column";
 import { Card } from "./Card";
 import { AppContainer } from "./styles";
 import { AddNewItem } from "./AddNewItem";
+import { useAppState } from "./AppStateContext";
 
 function App() {
+  const { state } = useAppState();
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Genrate app scaffold"></Card>
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript"></Card>
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing"></Card>
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text}></Column>
+      ))}
       <AddNewItem
         toggleButtonText="+Add another list"
         onAdd={console.log}
